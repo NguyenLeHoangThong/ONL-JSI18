afbdc56ead0b87cdd7f90b9b44d2a41a8d784c26
Thong Nguyen <thongnguyen@pixelcent.com>
2023-04-08 19:37:36 +0700

    First commit

---
+++ Bai1/github.js
@@ -0,0 +1,11 @@
+// git add .
+// git commit -m "Login feature"
+// git push
+// 3 lệnh đẩy code lên github
+
+
+// git pull
+// lệnh lấy code về
+
+// git clone
+// lệnh tải repo github của người khác về
